fc9a5543252994f4f212713086e9cf4147976603
Mariam <MariamGamal2110@>
2026-05-17 11:37:15 +0300

    Enable CORS for frontend integration

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import connectDB from "./config/db.js";
 
 import authRouter from "./routes/auth.routes.js";
 import profileRouter from "./routes/profile.routes.js";
 
 const app = express();
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 
